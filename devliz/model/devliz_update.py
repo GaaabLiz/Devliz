6cb3cff30128448c0ce3e5b56f010d25e17b9915
Gabriele <lizzosgabriele@gmail.com>
2025-10-17 19:10:53 +0200

    refactor: Simplify TaskGetMonitoredSoftware initialization and remove unused progress calculation

--- devliz/model/devliz_update.py
+++ devliz/model/devliz_update.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 from pylizlib.core.os.utils import is_software_installed, WindowsOsUtils
 from pylizlib.qt.handler.operation_core import Operation, Task
-from pylizlib.qt.handler.operation_domain import RunnerInteraction, OperationInfo
+from pylizlib.qt.handler.operation_domain import OperationInfo
 from pylizlib.qtfw.domain.sw import SoftwareData
 from qfluentwidgets import FluentIcon
 
@@ -12,14 +12,14 @@ from devliz.application.app import app_settings, DevlizSettings
 
 class TaskGetMonitoredSoftware(Task):
 
-    def __init__(self, name: str):
-        super().__init__(name)
+    def __init__(self):
+        super().__init__("Recupero Software Monitorati")
 
     def execute(self):
         data_list: list[str] = app_settings.get(DevlizSettings.starred_exes)
         data_objs: list[SoftwareData] = []
-        for i in range(1, 50000000):
-            progress = int((i / 50000000) * 100)
+        # for i in range(1, 50000000):
+        #     progress = int((i / 50000000) * 100)
             #self.update_task_progress(progress)
         for data in data_list:
             obj = SoftwareData(
