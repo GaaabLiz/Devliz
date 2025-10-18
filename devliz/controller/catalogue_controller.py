edb272d853f20d15fea2ecbd374ff7ecbae4475f
Gabriele <lizzosgabriele@gmail.com>
2025-10-18 17:14:24 +0200

    feat: Enhance CatalogueController with configuration dialog handling and signal integration

--- devliz/controller/catalogue_controller.py
+++ devliz/controller/catalogue_controller.py
@@ -1,6 +1,39 @@
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import QWidget
+from loguru import logger
+from pylizlib.core.os.snap import Snapshot
+from pylizlib.qtfw.util.ui import UiUtils
+
+from devliz.domain.data import DevlizData
+from devliz.view.util.frame import DevlizQFrame
+from devliz.view.widgets.catalogue import SnapshotCatalogueWidget
+from devliz.view.widgets.catalogue_imp_dialog import DialogConfig
 
 
 class CatalogueController:
 
-    def __init__(self, catalogue_widget):
+    def __init__(
+            self,
+            cached_data: DevlizData,
+            catalogue_widget: SnapshotCatalogueWidget,
+            signal_update: Signal | None = None
+    ):
+        self.cached_data: DevlizData | None = cached_data
         self.view = catalogue_widget
+        self.signal_update = signal_update
+
+    def init(self):
+        self.view.signal_import_requested.connect(lambda: self.__open_config_dialog(False, None))
+
+    def __open_config_dialog(self, edit_mode: bool, snap: Snapshot | None = None):
+        dialog = DialogConfig(self.cached_data, edit_mode, snap)
+        try:
+            if dialog.exec():
+                #ConfigManager.create(dialog.output_data, edit_mode)
+                titolo = "Configurazione creata" if not edit_mode else "Configurazione modificata"
+                testo = "La configurazione è stata creata con successo." if not edit_mode else "La configurazione è stata modificata con successo."
+                UiUtils.show_message(titolo, testo)
+                #self.signal_update.emit()
+        except Exception as e:
+            logger.error(str(e))
+            UiUtils.show_message("Attenzione", "Si è verificato un errore: " + str(e))
--- devliz/controller/dash_controller.py
+++ devliz/controller/dash_controller.py
@@ -1,5 +1,7 @@
+from PySide6.QtCore import Signal, QObject
 from loguru import logger
 from pylizlib.qt.domain.view import UiWidgetMode
+from pylizlib.qtfw.util.ui import UiUtils
 
 from devliz.controller.catalogue_controller import CatalogueController
 from devliz.domain.data import DevlizData, DevlizSnapshotData
@@ -9,24 +11,28 @@ from devliz.view.dash_view import DashboardView
 
 class DashboardController:
 
-    def __init__(self):
+    def __init__(self, /):
+        super().__init__()
         self.view = DashboardView()
         self.model = DashboardModel(self.view)
 
-        self.catalogue = CatalogueController(self.view.widget_setting)
+        self.cached_data : DevlizData | None = None
+
+        self.catalogue = CatalogueController(self.cached_data, self.view.widget_catalogue, None)
 
     def __update_all(self):
         try:
-            logger.info("Triggerato aggiornamento")
+            logger.debug("Triggerato update.")
             self.model.update()
         except Exception as e:
-            # UiUtils.show_message("Attenzione", "Si è verificato un errore durante l'aggiornamento dei dati di DEVLIZ: " + str(e))
+            UiUtils.show_message("Attenzione", "Si è verificato un errore durante l'aggiornamento dei dati di DEVLIZ: " + str(e))
             logger.error("Errore aggiornamento:" + str(e))
 
     def __handle_data_updated(self, data: DevlizData):
         print(data)
         snap_data = DevlizSnapshotData(snapshot_list=data.snapshots)
         self.view.widget_catalogue.update_widget(snap_data)
+        self.cached_data = data
 
     def __handle_update_started(self):
         self.view.set_state(UiWidgetMode.UPDATING)
@@ -35,6 +41,7 @@ class DashboardController:
         self.view.set_state(UiWidgetMode.DISPLAYING)
 
     def __connect_signals(self):
+        #self.signal_request_update.connect(self.__update_all)
         self.view.f5_pressed.connect(lambda: self.__update_all())
         self.model.signal_on_update_started.connect(self.__handle_update_started)
         self.model.signal_on_update_complete.connect(self.__handle_update_complete)
@@ -45,3 +52,4 @@ class DashboardController:
         self.view.show()
         self.__connect_signals()
         self.__update_all()
+        self.catalogue.init()
