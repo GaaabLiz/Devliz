93e545f4af52088294e9a8ae960cb73e5ec71b9b
Gabriele <lizzosgabriele@gmail.com>
2025-10-18 12:55:04 +0200

    refactor: Rename configurations to snapshots in DevlizSnapshotData for clarity

--- devliz/domain/data.py
+++ devliz/domain/data.py
@@ -7,16 +7,16 @@ from pylizlib.qtfw.domain.sw import SoftwareData
 
 @dataclass
 class DevlizSnapshotData:
-    configurations: list[Snapshot]
+    snapshot_list: list[Snapshot]
 
     @property
     def count(self) -> int:
-        return len(self.configurations)
+        return len(self.snapshot_list)
 
     @property
     def get_mb_size(self) -> str:
         total_size = 0
-        for config in self.configurations:
+        for config in self.snapshot_list:
             for dir_assoc in config.directories:
                 path = Path(dir_assoc.original_path)
                 if path.exists() and path.is_dir():
@@ -44,5 +44,5 @@ class DevlizData:
     monitored_software: list[SoftwareData] = None
     monitored_services: list[SoftwareData] = None
     settings: DevlizSettingsData = None
-    configurations: DevlizSnapshotData = None
+    snapshots: DevlizSnapshotData = None
 
