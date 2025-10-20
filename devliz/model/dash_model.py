93a1f110d5e9fba4eb27bd64b85daa810ecd90b0
Gabriele <lizzosgabriele@gmail.com>
2025-10-20 19:32:09 +0200

    feat: Integrate snapshot settings into SnapshotCatalogue initialization

--- devliz/model/dash_model.py
+++ devliz/model/dash_model.py
@@ -8,7 +8,7 @@ from pylizlib.qt.handler.operation_domain import OperationInfo
 from pylizlib.qt.handler.operation_runner import OperationRunner, RunnerStatistics
 from PySide6.QtCore import QObject, Signal
 
-from devliz.application.app import app_settings, DevlizSettings, DEVLIZ_PATH_BACKUPS
+from devliz.application.app import app_settings, DevlizSettings, DEVLIZ_PATH_BACKUPS, snap_settings
 from devliz.domain.data import DevlizData
 from devliz.model.devliz_update import TaskGetMonitoredSoftware, TaskGetSnapshots
 from devliz.view.dash_view import DashboardView
@@ -28,10 +28,7 @@ class DashboardModel(QObject):
         self.view = view
         self.snap_catalogue = SnapshotCatalogue(
             path_catalogue=Path(app_settings.get(DevlizSettings.catalogue_path)),
-            backup_path=DEVLIZ_PATH_BACKUPS,
-            backup_pre_install=app_settings.get(DevlizSettings.backup_before_install),
-            backup_pre_delete=False, # TODO
-            backup_pre_modify=False, # TODO
+            settings=snap_settings
         )
         self.task_monitored_soft = TaskGetMonitoredSoftware()
         self.task_snap = TaskGetSnapshots(self.snap_catalogue)
